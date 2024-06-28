4bde6d76cd093668f730fdc1b47c784c8e03bec0
abhiram2703 <abhirambiju2703@gmail.com>
2024-06-28 11:12:56 +0530

    Project Created

--- app/src/main/java/com/example/entrylog/MainActivity.java
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.entrylog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et1,et2;
+    AppCompatButton bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        et1=(EditText) findViewById(R.id.unameet);
+        et2=(EditText) findViewById(R.id.passet);
+        bt1=(AppCompatButton) findViewById(R.id.loginbtn);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=et1.getText().toString();
+                String s2=et2.getText().toString();
+                Toast.makeText(getApplicationContext(),s1+" "+s2+ " SUCCESSFUL",Toast.LENGTH_LONG).show();
+            }
         });
+
     }
 }
