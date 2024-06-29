ee976b781fc0d89992159f4ee3fc7e8cd63ffd70
abhiram2703 <abhirambiju2703@gmail.com>
2024-06-29 13:01:33 +0530

    Updated with networking

--- app/src/main/java/com/example/entrylog/LogEntry.java
+++ app/src/main/java/com/example/entrylog/LogEntry.java
@@ -14,9 +14,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LogEntry extends AppCompatActivity {
     EditText getName,getAdmNo,getSysNo,getDep;
     AppCompatButton addbtn,logoubtn;
+    String apiurl="http://10.0.4.16:3000/api/students";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +43,46 @@ public class LogEntry extends AppCompatActivity {
         addbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Reading values
                 String Name=getName.getText().toString();
                 String AdmNo=getAdmNo.getText().toString();
                 String SysNo=getSysNo.getText().toString();
                 String Dep=getDep.getText().toString();
-                Toast.makeText(getApplicationContext(),Name+" "+AdmNo+" "+SysNo+" "+Dep,Toast.LENGTH_LONG).show();
+
+                //Creating JSON object
+                JSONObject student=new JSONObject();
+                try {
+                    student.put("name",Name);
+                    student.put("admission_number",AdmNo);
+                    student.put("system_number",SysNo);
+                    student.put("department",Dep);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //JSON object request creation
+                JsonObjectRequest JsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,//METHOD parameter
+                        apiurl,//LINK parameter
+                        student,//DATA parameter
+                        //Response listener
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "Successfull", Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        //Error listener
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                );
+                //Request Queue
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(JsonObjectRequest);
             }
         });
         logoubtn.setOnClickListener(new View.OnClickListener() {
